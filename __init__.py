d12ff74df426ce116c5d01b8cfdb5a6c50816cf9
Rohan Gupta <simplyrohan10@gmail.com>
2023-06-29 13:59:27 -0700

    Code cleanup

--- __init__.py
+++ __init__.py
@@ -8,9 +8,6 @@ import pygame
 from math import asin, atan2, degrees
 from .loader import load
 
-AIR_THICKNESS = 1.25
-
-
 def bary_to_cart(bary, triangle):
     return (
         bary[0] * triangle[0][0] + bary[1] * triangle[1][0] + bary[2] * triangle[2][0],
@@ -41,7 +38,7 @@ def cart_to_bary(cart: tuple[int, int], triangle: list[tuple, tuple, tuple]):
 
 def get_pixel(point: tuple, triangle: list, uvs: list, texture: pygame.Surface):
     size = texture.get_size()
-    uvs = [(max(0, i[0]*size[0]-1), max(0, i[1]*size[1]-1)) for i in uvs]
+    uvs = [(max(0, i[0] * size[0] - 1), max(0, i[1] * size[1] - 1)) for i in uvs]
 
     bbary = cart_to_bary(point, triangle)
     if bbary == 0:
@@ -73,37 +70,26 @@ def rotate(vert, xyz) -> list:
     """
     Rotates given vertex on the X, Y, and Z dimensions.
     """
-    """
-    x, y, z = xyz
-    x_matrix = np.array([[1, 0, 0], [0, cos(x), -sin(x)], [0, sin(x), cos(x)]])
-
-    y_matrix = np.array([[cos(y), 0, sin(y)], [0, 1, 0], [-sin(y), 0, cos(y)]])
-
-    z_matrix = np.array([[cos(z), -sin(z), 0], [sin(z), cos(z), 0], [0, 0, 1]])
-
-    arr = np.array(vert)
-
-    return [round(i) for i in arr @ x_matrix @ y_matrix @ z_matrix]
-    """
-
     vert.rotate_x_ip(xyz[0])
     vert.rotate_y_ip(xyz[1])
     vert.rotate_z_ip(xyz[2])
 
 
-texture = pygame.image.load("test.jpeg").convert()
 class Model:
     def __init__(self, faces, scale) -> None:
         self.raw_faces = faces
 
         self.faces = [
-            [[axis * scale for axis in vertex[:3]]+vertex[-2:] for vertex in face]
+            [[axis * scale for axis in vertex[:3]] + vertex[-2:] for vertex in face]
             for face in self.raw_faces
         ]
         self.position = pygame.Vector3([0, 0, 0])
         self.rotation = pygame.Vector3([0, 0, 0])
 
         self.color = (54, 104, 143)
+
+        self.texture = pygame.Surface((1, 1))
+        self.texture.fill((150, 150, 150))
 
 
 class Camera:
@@ -130,7 +116,9 @@ class Camera:
     def render(self, objects, screen) -> None:
         # Transformations
         self.forward.normalize_ip()
-        pitch, yaw = degrees(asin(self.forward.y)), degrees(atan2(self.forward.x, self.forward.z))
+        pitch, yaw = degrees(asin(self.forward.y)), degrees(
+            atan2(self.forward.x, self.forward.z)
+        )
         print(yaw)
         faces = []
         for obj in objects:
@@ -147,7 +135,9 @@ class Camera:
                     true_point = pygame.Vector3(point)
                     rotate(true_point, obj.rotation)
                     # Position
-                    true_point: pygame.Vector3 = true_point + obj.position - self.position
+                    true_point: pygame.Vector3 = (
+                        true_point + obj.position - self.position
+                    )
 
                     # Camera transforms
                     # rotate(true_point, self.rotation)
@@ -156,48 +146,14 @@ class Camera:
                     zs.append(true_point[2])
 
                     true_face.append([a + 250 for a in self.project(true_point)])
-                faces.append([true_face, min(zs), uvs])
-                # color = obj.color
-                # pygame.draw.polygon(screen, color, true_face)
+                faces.append([true_face, min(zs), uvs, obj.texture])
+
         faces = sorted(faces, key=lambda v: v[1], reverse=True)
-        for face, z, uvs in faces:
+        for face, z, uvs, texture in faces:
             if z > -self.focal_length:
                 # print(uvs)
                 draw(screen, face, uvs, texture)
-                # pygame.draw.polygon(screen, color, face)
-                # pygame.draw.polygon(screen, "black", face, 2)
-                # highest_z = min(zs)
-
-                # faces.append([true_face, highest_z])
-
-        # Z-Sorting
-        # faces = sorted(faces, key=lambda face: face[1], reverse=True)
-
-    """
-        for face, highest_z in faces:
-            # Projection
-            projected_points = [[i + 250 for i in self.project(p)] for p in face]
-
-            # Shading
-            color = obj.color
-
-            light = [0, 0, 100]  # Light Source
-
-            mpoint = [0, 0, 0]  # Average of all points
-            for point in face:
-                mpoint = [v + point[i] for i, v in enumerate(point)]
-            mpoint = [i / len(face) for i in mpoint]
-
-            dist = math.dist(mpoint, light) - 120
-
-            dist *= AIR_THICKNESS  # Light to shade factor
-
-            color = [pygame.math.clamp(i + dist, 0, 200) for i in color]
-
-            # Drawing
-            pygame.draw.polygon(screen, color, projected_points)
-    """
-
+                
     def project(self, point: list[int, int, int]) -> tuple:
         # TODO: Do not use try/except to sole ZeroDiv. It is slow. Implement culling
         x_projected: float = (point[0] * self.focal_length) // (
